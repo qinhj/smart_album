b76bdc137e675ba0770817849d6fd091c2bbdfc1
qinhongjie <qinhj@lsec.cc.ac.cn>
2024-02-19 16:09:52 +0800

    [gui] uis: review pages and fix some issues

--- gui/uis/pages/ui_main_pages.py
+++ gui/uis/pages/ui_main_pages.py
@@ -29,120 +29,99 @@ class Ui_MainPages(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
 
-
-
         self.horizontalLayout.addStretch()
-        #############################################
 
-        self.func_1 = QWidget()
+        self.func_1 = QWidget(self.page_1)
         self.func_1.setObjectName(u"func_1")
         self.func_1.setStyleSheet(u"background: transparent;")
         self.func_1_layout = QVBoxLayout(self.func_1)
-        self.func_1_layout.setObjectName("func_1_layout")
-
-        self.func_1_lable = QLabel()
-        self.func_1_lable.setObjectName(u"func_1_lable")
-        self.func_1_lable.setStyleSheet(u"background: transparent;")
-        self.func_1_lable.setText("人脸分类")
-        self.func_1_lable.setStyleSheet(u"font-family:Microsoft Yahei;font-size: 20pt;color:white")
-        self.func_1_lable.setAlignment(Qt.AlignCenter)
-
-
-        self.func_1_frame_1 = QFrame()
-        self.func_1_frame_1.setObjectName(u"func_1_frame_1")
-        self.func_1_frame_1.setStyleSheet(u"background: transparent;")
-        self.func_1_frame_1_layout = QHBoxLayout(self.func_1_frame_1)
-
-
-        self.func_1_frame_2 = QFrame()
-        self.func_1_frame_2.setObjectName(u"func_1_frame_2")
-        self.func_1_frame_2.setStyleSheet(u"background: transparent;")
-        self.func_1_frame_2_layout = QHBoxLayout(self.func_1_frame_2)
+        self.func_1_layout.setSpacing(50)
+        self.func_1_layout.setObjectName(u"func_1_layout")
+        self.func_1_label = QLabel(self.func_1)
+        self.func_1_label.setObjectName(u"func_1_label")
+        self.func_1_label.setStyleSheet(u"background: transparent;font-family: Microsoft Yahei;font-size: 20pt;color: white;")
+        self.func_1_label.setAlignment(Qt.AlignCenter)
 
         self.func_1_layout.addStretch()
-        self.func_1_layout.addWidget(self.func_1_lable)
+        self.func_1_layout.addWidget(self.func_1_label)
+
+        self.func_1_frame_1 = QFrame(self.func_1)
+        self.func_1_frame_1.setObjectName(u"func_1_frame_1")
+        self.func_1_frame_1.setStyleSheet(u"background: transparent;")
+
         self.func_1_layout.addWidget(self.func_1_frame_1)
+
+        self.func_1_frame_2 = QFrame(self.func_1)
+        self.func_1_frame_2.setObjectName(u"func_1_frame_2")
+        self.func_1_frame_2.setStyleSheet(u"background: transparent;")
+
         self.func_1_layout.addWidget(self.func_1_frame_2)
         self.func_1_layout.addStretch()
 
-        self.func_1_layout.setSpacing(50)
         self.horizontalLayout.addWidget(self.func_1)
-        ##################################################################
-        self.func_2 = QWidget()
+
+        self.func_2 = QWidget(self.page_1)
         self.func_2.setObjectName(u"func_2")
         self.func_2.setStyleSheet(u"background: transparent;")
         self.func_2_layout = QVBoxLayout(self.func_2)
-        self.func_2_layout.setObjectName("func_2_layout")
-
-        self.func_2_lable = QLabel()
-        self.func_2_lable.setObjectName(u"func_2_lable")
-        self.func_2_lable.setStyleSheet(u"background: transparent;")
-        self.func_2_lable.setText("人脸搜索")
-        self.func_2_lable.setStyleSheet(u"font-family:Microsoft Yahei;font-size: 20pt;color:white")
-        self.func_2_lable.setAlignment(Qt.AlignCenter)
-
-
-        self.func_2_frame_1 = QFrame()
-        self.func_2_frame_1.setObjectName(u"func_2_frame_1")
-        self.func_2_frame_1.setStyleSheet(u"background: transparent;")
-        self.func_2_frame_1_layout = QHBoxLayout(self.func_2_frame_1)
-
-
-        self.func_2_frame_2 = QFrame()
-        self.func_2_frame_2.setObjectName(u"func_2_frame_2")
-        self.func_2_frame_2.setStyleSheet(u"background: transparent;")
-        self.func_2_frame_2_layout = QHBoxLayout(self.func_2_frame_2)
+        self.func_2_layout.setSpacing(50)
+        self.func_2_layout.setObjectName(u"func_2_layout")
+        self.func_2_label = QLabel(self.func_2)
+        self.func_2_label.setObjectName(u"func_2_label")
+        self.func_2_label.setStyleSheet(u"background: transparent;font-family: Microsoft Yahei;font-size: 20pt;color: white;")
+        self.func_2_label.setAlignment(Qt.AlignCenter)
 
         self.func_2_layout.addStretch()
-        self.func_2_layout.addWidget(self.func_2_lable)
+        self.func_2_layout.addWidget(self.func_2_label)
+
+        self.func_2_frame_1 = QFrame(self.func_2)
+        self.func_2_frame_1.setObjectName(u"func_2_frame_1")
+        self.func_2_frame_1.setStyleSheet(u"background: transparent;")
+
         self.func_2_layout.addWidget(self.func_2_frame_1)
+
+        self.func_2_frame_2 = QFrame(self.func_2)
+        self.func_2_frame_2.setObjectName(u"func_2_frame_2")
+        self.func_2_frame_2.setStyleSheet(u"background: transparent;")
+
         self.func_2_layout.addWidget(self.func_2_frame_2)
         self.func_2_layout.addStretch()
 
-        self.func_2_layout.setSpacing(50)
         self.horizontalLayout.addWidget(self.func_2)
-        ######################################################################
 
-        self.func_3 = QWidget()
+        self.func_3 = QWidget(self.page_1)
         self.func_3.setObjectName(u"func_3")
         self.func_3.setStyleSheet(u"background: transparent;")
         self.func_3_layout = QVBoxLayout(self.func_3)
-        self.func_3_layout.setObjectName("func_3_layout")
-
-        self.func_3_lable = QLabel()
-        self.func_3_lable.setObjectName(u"func_3_lable")
-        self.func_3_lable.setStyleSheet(u"background: transparent;")
-        self.func_3_lable.setText("智能筛重")
-        self.func_3_lable.setStyleSheet(u"font-family:Microsoft Yahei;font-size: 20pt;color:white")
-        self.func_3_lable.setAlignment(Qt.AlignCenter)
-
-
-        self.func_3_frame_1 = QFrame()
-        self.func_3_frame_1.setObjectName(u"func_3_frame_1")
-        self.func_3_frame_1.setStyleSheet(u"background: transparent;")
-        self.func_3_frame_1_layout = QHBoxLayout(self.func_3_frame_1)
-
-
-        self.func_3_frame_2 = QFrame()
-        self.func_3_frame_2.setObjectName(u"func_3_frame_2")
-        self.func_3_frame_2.setStyleSheet(u"background: transparent;")
-        self.func_3_frame_2_layout = QHBoxLayout(self.func_3_frame_2)
+        self.func_3_layout.setSpacing(50)
+        self.func_3_layout.setObjectName(u"func_3_layout")
+        self.func_3_label = QLabel(self.func_3)
+        self.func_3_label.setObjectName(u"func_3_label")
+        self.func_3_label.setStyleSheet(u"background: transparent;font-family: Microsoft Yahei;font-size: 20pt;color: white;")
+        self.func_3_label.setAlignment(Qt.AlignCenter)
 
         self.func_3_layout.addStretch()
-        self.func_3_layout.addWidget(self.func_3_lable)
+        self.func_3_layout.addWidget(self.func_3_label)
+
+        self.func_3_frame_1 = QFrame(self.func_3)
+        self.func_3_frame_1.setObjectName(u"func_3_frame_1")
+        self.func_3_frame_1.setStyleSheet(u"background: transparent;")
+
         self.func_3_layout.addWidget(self.func_3_frame_1)
+
+        self.func_3_frame_2 = QFrame(self.func_3)
+        self.func_3_frame_2.setObjectName(u"func_3_frame_2")
+        self.func_3_frame_2.setStyleSheet(u"background: transparent;")
+
         self.func_3_layout.addWidget(self.func_3_frame_2)
         self.func_3_layout.addStretch()
 
 
-        self.func_3_layout.setSpacing(50)
         self.horizontalLayout.addWidget(self.func_3)
-        #################################################################
 
         self.horizontalLayout.addStretch()
 
         self.pages.addWidget(self.page_1)
-
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
         self.page_2_layout = QVBoxLayout(self.page_2)
@@ -157,8 +136,8 @@ class Ui_MainPages(object):
 "}")
         self.page_3_layout = QVBoxLayout(self.page_3)
         self.page_3_layout.setObjectName(u"page_3_layout")
-        self.scrollArea_1= QScrollArea(self.page_3)
-        self.scrollArea_1.setObjectName(u"scrollArea")
+        self.scrollArea_1 = QScrollArea(self.page_3)
+        self.scrollArea_1.setObjectName(u"scrollArea_1")
         self.scrollArea_1.setStyleSheet(u"background: transparent;")
         self.scrollArea_1.setFrameShape(QFrame.NoFrame)
         self.scrollArea_1.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
@@ -176,7 +155,6 @@ class Ui_MainPages(object):
         self.page_3_layout.addWidget(self.scrollArea_1)
 
         self.pages.addWidget(self.page_3)
-        ############################################################################
         self.page_4 = QWidget()
         self.page_4.setObjectName(u"page_4")
         self.page_4.setStyleSheet(u"QFrame {\n"
@@ -194,7 +172,6 @@ class Ui_MainPages(object):
         self.page_4_layout.addWidget(self.scrollArea_2)
 
         self.pages.addWidget(self.page_4)
-        #############################################################################
         self.page_5 = QWidget()
         self.page_5.setObjectName(u"page_5")
         self.page_5.setStyleSheet(u"QFrame {\n"
@@ -212,9 +189,9 @@ class Ui_MainPages(object):
         self.page_5_layout.addWidget(self.scrollArea_3)
 
         self.pages.addWidget(self.page_5)
-        #############################################################################
 
         self.main_pages_layout.addWidget(self.pages)
+
 
         self.retranslateUi(MainPages)
 
@@ -226,5 +203,8 @@ class Ui_MainPages(object):
 
     def retranslateUi(self, MainPages):
         MainPages.setWindowTitle(QCoreApplication.translate("MainPages", u"Form", None))
+        self.func_1_label.setText(QCoreApplication.translate("MainPages", u"\u4eba\u8138\u5206\u7c7b", None)) # 人脸分类
+        self.func_2_label.setText(QCoreApplication.translate("MainPages", u"\u4eba\u8138\u641c\u7d22", None)) # 人脸搜索
+        self.func_3_label.setText(QCoreApplication.translate("MainPages", u"\u667a\u80fd\u7b5b\u9009", None)) # 智能筛选
     # retranslateUi
 
