0792ce00031c6e83e97ed846743689a735954d7e
qinhongjie <qinhj@lsec.cc.ac.cn>
2024-02-26 14:02:15 +0800

    [backend] code review and refactor

--- backend/backend.py
+++ backend/backend.py
@@ -6,9 +6,11 @@
 
 # IMPORT PACKAGES AND MODULES
 # ///////////////////////////////////////////////////////////////
-import os, sys
+import os
 from abc import ABC, abstractmethod
-from backend.worker import create_detect_worker, create_search_worker, create_duplicate_worker
+from backend.worker import create_worker_face_cluster
+from backend.worker import create_worker_image_search
+from backend.worker import create_worker_image_similarity
 
 # IMPORT QT CORE
 # ///////////////////////////////////////////////////////////////
@@ -35,16 +37,28 @@ class FakeBackend(IBackend):
         super().__init__(main_window)
         self._image_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "assets", "obama")
 
-        def fake_detect(image_paths):
+        def fake_face_cluster(image_dir):
+            persons = {}
+            copyright_label = self._main_window.ui.credits.copyright_label
+
+            from backend.utils import get_image_paths, differ_paths
+            image_paths = get_image_paths(self._main_window.settings["image_path"])
+            image_paths = differ_paths(image_paths, self._main_window.settings["output_path"])
+            total_image = len(image_paths)
+            if total_image == 0:
+                copyright_label.setText("共有 0 张新增图片，识别取消")
+                return persons
+            # DEBUG
+            print("[DEBUG] Found {} images, start face cluster ...".format(total_image))
+
             from backend.display_by_person import load_json, write_json
             results = load_json(main_window.settings["output_path"])
-            persons = {}
             for item in results:
                 k, v = item["person"], item["pic_name"]
                 if k in persons.keys():
-                    persons[k].append(item["pic_name"])
+                    persons[k].append(v)
                 else:
-                    persons[k] = [item["pic_name"]]
+                    persons[k] = [v]
             if u"未命名" not in persons.keys():
                 persons[u"未命名"] = image_paths
             else:
@@ -52,15 +66,15 @@ class FakeBackend(IBackend):
             write_json(persons)
             return {u"未命名": image_paths} # just do nothing
 
-        def fake_search(image_path):
-            print("[WARN] fake search will ignore input images!")
+        def fake_image_search(image_args):
+            print("[WARN] fake image search will ignore input images!")
             # always reset settings['image_path'] as test data dir
             main_window.settings['image_path'] = self._image_dir
             from backend.utils import get_image_paths
             return {"obama": get_image_paths(self._image_dir)}
 
-        def fake_duplicate(image_dir):
-            print("[WARN] fake duplicate will ignore input image directory!")
+        def fake_image_similarity(image_dir):
+            print("[WARN] fake image similarity will ignore input image directory!")
             fake_result = [
                 os.path.normpath(os.path.join(self._image_dir, "Obama_00.jpg")),
                 os.path.normpath(os.path.join(self._image_dir, "Obama_00.copy.jpg"))
@@ -69,9 +83,9 @@ class FakeBackend(IBackend):
 
         # create handler for supported task and thread worker
         self._task_handler = {
-            "Detect" : [create_detect_worker, fake_detect],
-            "Search" : [create_search_worker, fake_search],
-            "Duplicate" : [create_duplicate_worker, fake_duplicate],
+            "face_cluster" : [create_worker_face_cluster, fake_face_cluster],
+            "image_search" : [create_worker_image_search, fake_image_search],
+            "image_similarity" : [create_worker_image_similarity, fake_image_similarity],
         }
 
     def __call__(self, cmd: str, *args):
@@ -85,4 +99,7 @@ class FakeBackend(IBackend):
 def backend_selector(qw: QWidget, name: str):
     if name.lower() == "fake":
         return FakeBackend(qw)
+    if name.lower() == "tibor":
+        from backend.tibor.api import TiborBackend
+        return TiborBackend(qw)
     raise RuntimeError("Unsupported backend: {}".format(name))
--- backend/worker.py
+++ backend/worker.py
@@ -7,7 +7,6 @@
 # IMPORT PACKAGES AND MODULES
 # ///////////////////////////////////////////////////////////////
 import time
-from backend.utils import get_image_paths, differ_paths
 
 # IMPORT QT CORE
 # ///////////////////////////////////////////////////////////////
@@ -27,120 +26,99 @@ class Worker(QThread):
         self.finished.emit(result)
 
 
-# Default worker for detection task
-def create_detect_worker(main_window: QMainWindow, worker_handler, *args):
+class TimerLabel(QTimer):
+    def __init__(self, label):
+        super().__init__()
+        self._label = label
+        self._count = 0
+        self._start = time.time()
+        #self._label.setText(u"正在分析中，请稍等")
+
+        def slot_update_text():
+            self._count = int(time.time() - self._start)
+            self._label.setText(u"智能分析中, 已开始 {}s".format(self._count))
+        self.timeout.connect(lambda: slot_update_text())
+
+    def stop(self):
+        super().stop()
+        self._count = int(time.time() - self._start)
+        self._label.setText(u"智能分析完成, 总耗时 {}s".format(self._count))
+
+
+_TEXT_NEED_IMAGE_DIR = u"还未选择图片文件夹，请先选择文件夹"
+_TEXT_FILL_BLANK = "                                    "
+
+# Default worker for face cluster task
+def create_worker_face_cluster(main_window: QMainWindow, worker_handler, *args):
     if worker_handler is None:
-        raise RuntimeError("Input worker handler for face detection task is None!")
+        raise RuntimeError("Input worker handler for face cluster task is None!")
 
     if main_window.settings["image_path"] == "":
-        QMessageBox.information(main_window, main_window.settings["app_name"], "还未选择图片文件夹，请先选择文件夹后再开始识别")
+        QMessageBox.information(main_window, main_window.settings["app_name"], _TEXT_NEED_IMAGE_DIR)
         return None
 
-    copyright_label = main_window.ui.credits.copyright_label
-
-    image_paths = get_image_paths(main_window.settings["image_path"])
-    image_paths = differ_paths(image_paths, main_window.settings["output_path"])
-    total_image = len(image_paths)
-    if total_image == 0:
-        copyright_label.setText("共有 0 张新增图片，识别取消")
-        return None
-    # DEBUG
-    print("[DEBUG] Found {} images, start detection and recognition ...".format(total_image))
-
-    timer_count = 0
-    timer_start = time.time()
-    timer = QTimer()
-    
-    def detect_print_time():
-        timer_count = int(time.time() - timer_start)
-        copyright_label.setText("正在识别中，已开始 {} 秒".format(timer_count))
-        #print("正在识别中，{}/{}，已识别 {} 秒".format(processed_image, total_image, timer_count))
-
-    timer.timeout.connect(lambda: detect_print_time())
+    timer = TimerLabel(main_window.ui.credits.copyright_label)
     timer.start(100)
 
-    def detect_finished(result):
+    def cluster_finished(result):
         timer.stop()
-        main_window.classification_result = result
-        copyright_label.setText("完成识别，耗时 {} 秒".format(timer_count))
-        QMessageBox.information(main_window, main_window.settings["app_name"], "完成识别")
+        main_window.face_cluster_result = result
+        QMessageBox.information(main_window, main_window.settings["app_name"], u"人脸聚类完成{}".format(_TEXT_FILL_BLANK))
 
-    main_window.worker_detect = Worker(worker_handler, image_paths)
-    main_window.worker_detect.finished.connect(detect_finished)
-    main_window.worker_detect.start()
+    main_window.face_cluster_worker = Worker(worker_handler, main_window.settings["image_path"])
+    main_window.face_cluster_worker.finished.connect(cluster_finished)
+    main_window.face_cluster_worker.start()
 
 
-# Default worker for face search task
-def create_search_worker(main_window: QMainWindow, worker_handler, *args):
+# Default worker for image search task
+def create_worker_image_search(main_window: QMainWindow, worker_handler, *args):
     if worker_handler is None:
-        raise RuntimeError("Input worker handler for face search task is None!")
+        raise RuntimeError("Input worker handler for image search task is None!")
 
     if main_window.settings["image_path"] == "":
-        QMessageBox.information(main_window, main_window.settings["app_name"], "还未选择图片文件夹，请先选择文件夹后再开始识别")
+        QMessageBox.information(main_window, main_window.settings["app_name"], _TEXT_NEED_IMAGE_DIR)
         return None
 
-    copyright_label = main_window.ui.credits.copyright_label
-    #copyright_label.setText("正在进行人脸搜索，请稍等")
-
-    timer_count = 0
-    timer_start = time.time()
-    timer = QTimer()
-
-    def search_print_time():
-        timer_count = int(time.time() - timer_start)
-        copyright_label.setText("正在搜索中，已开始 {} 秒".format(timer_count))
-
-    timer.timeout.connect(lambda: search_print_time())
+    timer = TimerLabel(main_window.ui.credits.copyright_label)
     timer.start(100)
 
     def search_finished(result):
         timer.stop()
-        main_window.person_search_result = result
-        copyright_label.setText("搜索完成，耗时 {} 秒".format(timer_count))
-        QMessageBox.information(main_window, main_window.settings["app_name"], "搜索完成")
+        main_window.image_search_result = result
+        QMessageBox.information(main_window, main_window.settings["app_name"], u"智能搜图完成{}".format(_TEXT_FILL_BLANK))
 
     if main_window.selected_image is None or main_window.selected_image == '':
-        QMessageBox.information(main_window, main_window.settings["app_name"], "还未选择照片，请先选择照片后再开始搜索")
+        QMessageBox.information(
+            main_window, main_window.settings["app_name"],
+            u"还未选择图片或输入文本，请先选择图片或输入文本后再执行")
     else:
-        # TODO: decouple with input QWidget
-        main_window.search_changed = True
-        main_window.has_searched = True
-        main_window.worker_search = Worker(worker_handler, main_window.selected_image)
-        main_window.worker_search.finished.connect(search_finished)
-        main_window.worker_search.start()
+        # TODO: decouple with input main_window?
+        main_window.image_search_changed = True
+        main_window.image_search_worker = Worker(worker_handler, [main_window.settings["image_path"], main_window.selected_image])
+        main_window.image_search_worker.finished.connect(search_finished)
+        main_window.image_search_worker.start()
+        main_window.image_search_done = True
 
 
-# Default worker for image duplicate task
-def create_duplicate_worker(main_window: QMainWindow, worker_handler, *args):
+# Default worker for image similarity task
+def create_worker_image_similarity(main_window: QMainWindow, worker_handler, *args):
     if worker_handler is None:
-        raise RuntimeError("Input worker handler for duplicate task is None!")
+        raise RuntimeError("Input worker handler for image similarity task is None!")
 
     if main_window.settings["image_path"] == "":
-        QMessageBox.information(main_window, main_window.settings["app_name"], "还未选择图片文件夹，请先选择文件夹后再开始识别")
+        QMessageBox.information(main_window, main_window.settings["app_name"], _TEXT_NEED_IMAGE_DIR)
         return None
 
-    copyright_label = main_window.ui.credits.copyright_label
-    #copyright_label.setText("正在进行相似图片筛查，请稍等")
-
-    timer_start = time.time()
-    timer_count = 0
-    timer = QTimer()
-
-    def dedup_print_time():
-        timer_count = int(time.time() - timer_start)
-        copyright_label.setText("正在筛查中，已开始 {} 秒".format(timer_count))
-
-    timer.timeout.connect(lambda: dedup_print_time())
+    timer = TimerLabel(main_window.ui.credits.copyright_label)
     timer.start(100)
 
-    def dedup_finished(path):
+    def similarity_finished(path):
         timer.stop()
-        copyright_label.setText("筛查完成，耗时 {} 秒".format(timer_count))
-        main_window.person_duplicate_result = path
+        main_window.image_similarity_result = path
         main_window.image_pages = []
-        QMessageBox.information(main_window, main_window.settings["app_name"], "完成筛查")
+        QMessageBox.information(main_window, main_window.settings["app_name"], u"智能筛重完成{}".format(_TEXT_FILL_BLANK))
 
-    main_window.found_duplicate_image = True
-    main_window.worker_duplicate = Worker(worker_handler, main_window.settings['image_path'])
-    main_window.worker_duplicate.finished.connect(dedup_finished)
-    main_window.worker_duplicate.start()
+    main_window.image_similarity_worker = Worker(worker_handler, main_window.settings['image_path'])
+    main_window.image_similarity_worker.finished.connect(similarity_finished)
+    main_window.image_similarity_worker.start()
+    main_window.image_similarity_done = True
